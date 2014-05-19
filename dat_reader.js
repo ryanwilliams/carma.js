0e577d1df98a73cc8efcb9751da7630d2da896fa
Ryan Williams <ryan@ryanwillia.ms>
2014-05-19 15:51:55 +1200

    Some clumsy parsing of header record

--- dat_reader.js
+++ dat_reader.js
@@ -1,24 +1,133 @@
-(function () {
+window.carmageddon = (function () {
+  // When input changes, load the file!
   var datFileInput = document.querySelector('#dat-file-input');
-  datFileInput.addEventListener('change', onDatFileChange);
+  datFileInput.addEventListener('change', function (e) {
+    console.log("Dat FILE CHANGED!", e);
+    var reader = new DatFileReader({file: e.target.files[0]});
+  });
 
+  var BYTE  = 1,
+      INT   = 2,
+      WORD  = 3,
+      FLOAT = 4;
 
-  function onDatFileChange (e) {
-    console.log("DAT FILE CHANGED!", e);
-    var reader = new DATReader(e.target.files[0]);
+  var HEADER     = 0x12,
+      MODEL_NAME = 0x36,
+      VERTICIES  = 0x17,
+      TEX_COORDS = 0x18,
+      FACES      = 0x35,
+      MAT_NAMES  = 0x16,
+      MAT_FACES  = 0x1A;
+
+  // How many bytes and which methods to use for each value type
+  var READ_MAP = {};
+  READ_MAP[BYTE]  = [1, 'Uint8'];
+  READ_MAP[INT]   = [2, 'Uint16'];
+  READ_MAP[WORD]  = [4, 'Uint32'];
+  READ_MAP[FLOAT] = [4, 'Float32'];
+
+  function DatModel () {
+    this.meshes = [];
+    this.vertices = [];
+    this.uvs = [];
+    this.faces = [];
+    this.materialNames = [];
+    this.faceMaterials = [];
+  }
+
+  function Record (buffer, offset) {
+    this.cursor = 0;
+
+    var view = new DataView(buffer, offset);
+
+    var type = view.getUint32(0),
+        length = view.getUint32(4);
+
+    if (length > buffer.byteLength - offset) {
+      alert("Read past end of buffer; aborting");
+      return;
+    }
+
+    this.type = type;
+    this.byteLength = length;
+
+    // Offset by 8 bytes to skip the record header
+    this.data = new DataView(buffer, offset + 8, length);
   }
 
 
-  function DATReader (file) {
-    console.log("READING", file);
-    this.file = file;
-    this.fileReader = new FileReader();
-    this.fileReader.addEventListener('load', this.onFileLoad.bind(this));
-    this.fileReader.readAsArrayBuffer(this.file);
-  }
+  Record.prototype.readValue = function (type) {
+    var value = this.data['get' + READ_MAP[type][1]](this.cursor);
+    this.cursor += READ_MAP[type][0];
 
-  DATReader.prototype.onFileLoad = function (e) {
-    console.log("FILE LOADED!", this.fileReader.result.byteLength);
+    return value;
   };
 
+  Record.prototype.readByte = function () {
+    return this.readValue(BYTE);
+  };
+
+  Record.prototype.readInt = function () {
+    return this.readValue(INT);
+  };
+
+  Record.prototype.readWord = function () {
+    return this.readValue(WORD);
+  };
+
+  Record.prototype.readFloat = function () {
+    return this.readValue(FLOAT);
+  };
+
+
+
+
+
+  function DatFileReader (options) {
+    options || (options = {});
+
+    this.datModel = new DatModel();
+    this.cursor = 0;
+
+    if (options.file) {
+      console.log("READING", options.file);
+      this.file = options.file;
+      this.fileReader = new FileReader();
+      this.fileReader.addEventListener('load', this.onFileLoad.bind(this));
+      this.fileReader.readAsArrayBuffer(this.file);
+    }
+  }
+
+  DatFileReader.prototype.onFileLoad = function (e) {
+    console.log("FILE LOADED!", this.fileReader.result.byteLength);
+    this.data = new DataView(this.fileReader.result);
+    this.parseArrayBuffer();
+  };
+
+  DatFileReader.prototype.parseArrayBuffer = function () {
+    this.cursor = 0;
+
+    var header = this.readNextRecord();
+
+    if (header.type != HEADER) {
+      alert("I have no idea how to read this file");
+      return;
+    }
+
+    if (header.readWord() != 0xFACE) {
+      alert("This isn't a DAT file.");
+      return;
+    }
+
+    alert(JSON.stringify(this.datModel, null, '    '));
+  };
+
+  DatFileReader.prototype.readNextRecord = function () {
+    return new Record(this.data.buffer, this.cursor);
+  };
+
+  return {
+    DatModel: DatModel,
+    DatFileReader: DatFileReader
+  };
 })();
